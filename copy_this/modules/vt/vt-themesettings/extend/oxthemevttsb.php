7269209fd588f487f146fe4442ec77e7bde2b103
Marat <vanilla-thunder@users.noreply.github.com>
2016-11-30 15:11:23 +0100

    Add files via upload

--- copy_this/modules/vt/vt-themesettings/extend/oxthemevttsb.php
+++ copy_this/modules/vt/vt-themesettings/extend/oxthemevttsb.php
@@ -6,7 +6,7 @@ class oxthemevttsb extends oxthemevttsb_parent
    {
       try {
          parent::activate();
-
+         $oDb = oxDb::getDb();
          $oUtils = oxRegistry::getUtils();
          $oUtils->resetLanguageCache();
 
@@ -16,14 +16,19 @@ class oxthemevttsb extends oxthemevttsb_parent
             $oFakeModule->setModuleData($this->_aTheme);
             //print_r($this->_aTheme);
 
+            // cleanup settings
+            $oDb->execute("DELETE FROM oxconfig WHERE OXMODULE = 'theme:" . $this->getId() . "'");
+            $oDb->execute("DELETE FROM oxconfigdisplay WHERE OXCFGMODULE = 'theme:" . $this->getId() . "'");
+
             /** @var oxModuleInstaller $oModuleInstaller */
             $oModuleInstaller = oxNew('oxModuleInstaller');
             if ($oModuleInstaller->activate($oFakeModule)) {
                $settings = "'" . implode("','", array_column($this->getInfo('settings'), 'name')) . "'";
+
                $oxConfigSql = "UPDATE oxconfig SET OXMODULE = REPLACE(OXMODULE,'module:" . $this->getId() . "','theme:" . $this->getId() . "') WHERE OXMODULE = 'module:" . $this->getId() . "' AND OXVARNAME IN(" . $settings . ") ";
-               $oxConfigDisplaySql = "UPDATE oxconfigdisplay SET OXCFGMODULE = REPLACE(OXCFGMODULE,'module:" . $this->getId() . "','theme:" . $this->getId() . "') WHERE OXCFGMODULE = 'module:" . $this->getId() . "' AND OXCFGVARNAME IN(" . $settings . ") ";
-               $oDb = oxDb::getDb();
                $oDb->execute($oxConfigSql);
+
+               $oxConfigDisplaySql = "UPDATE oxconfigdisplay SET OXCFGMODULE = REPLACE(OXCFGMODULE,'module:" . $this->getId() . "','theme:" . $this->getId() . "') WHERE OXCFGMODULE = 'module:" . $this->getId() . "' AND OXCFGVARNAME IN(" . $settings . ") ";
                $oDb->execute($oxConfigDisplaySql);
                //var_dump();
             }
