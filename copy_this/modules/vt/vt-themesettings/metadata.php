ff8972f403c2c9a1e446d863bd9401ba690768b6
Marat <m@marat.ws>
2017-01-13 14:23:48 +0100

    updates

--- copy_this/modules/vt/vt-themesettings/metadata.php
+++ copy_this/modules/vt/vt-themesettings/metadata.php
@@ -1,46 +1,36 @@
 <?php
+
 /**
- * [vt] theme settings backport for OXID 4.9 - 4.10
- * The MIT License (MIT)
+ * importing Theme Settings from theme.php for OXID eShop
+ * Copyright (C) 2017  Marat Bedoev
+ * info:  m@marat.ws
  *
- * Copyright (C) 2016  Marat Bedoev
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * Author:     Marat Bedoev <m@marat.ws>
+ * author: Marat Bedoev
  */
 
 $sMetadataVersion = '1.1';
-$aModule = array(
+$aModule = [
     'id'          => 'vt-themesettings',
-    'title'       => '[vt] theme settings backport',
-    'description' => 'backport for the new exciting OXID 6 feature: theme.php settings',
+    'title'       => '[vt] theme settings',
+    'description' => '___DESCRIPTION___',
     'thumbnail'   => 'oxid-vt.jpg',
-    'version'     => '0.0.1',
+    'version'     => '0.0.1 2017-1-13',
     'author'      => 'Marat Bedoev',
     'email'       => 'm@marat.ws',
     'url'         => 'https://github.com/vanilla-thunder/vt-themesettings',
-    'extend'      => array(
-       'oxtheme' => 'vt/vt-themesettings/extend/oxthemevttsb',
-       'theme_config' => 'vt/vt-themesettings/extend/theme_configvttsb'
-    ),
-    'templates' => [
-       'vt_themesettings.tpl'  => 'vt/vt-themesettings/application/views/admin/vt_themesettings.tpl'
+    'extend'      => [
+        'oxtheme'      => 'vt/vt-themesettings/extend/oxthemevttsb',
+        'theme_config' => 'vt/vt-themesettings/extend/theme_configvttsb'
+    ],
+    'templates'   => [
+        'vt_themesettings.tpl' => 'vt/vt-themesettings/application/views/admin/vt_themesettings.tpl'
     ]
-);
+];
