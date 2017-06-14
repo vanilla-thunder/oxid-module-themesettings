987e2d9a688b035a4e5cf1bd1c3cb1af0b84ab51
Marat <vanilla-thunder@users.noreply.github.com>
2017-06-14 15:33:20 +0200

    Create 1_init.js

--- 1_init.js
+++ 1_init.js
@@ -1,13 +1,13 @@
 var runner = require('child_process');
 
-runner.exec("git clone git@github.com:vanilla-thunder/vt-themesettings.git _master --depth 1",
+runner.exec("git clone git@github.com:vanilla-thunder/themesettings.git _master --depth 1",
     function (err, stdout, stderr) {
         if(err) console.log(err);
         else if(stderr) console.log(stderr);
         else console.log("master ok");
     }
 );
-runner.exec("git clone -b module git@github.com:vanilla-thunder/vt-themesettings.git _module --depth 1",
+runner.exec("git clone -b module git@github.com:vanilla-thunder/themesettings.git _module --depth 1",
     function (err, stdout, stderr) {
         if(err) console.log(err);
         else if(stderr) console.log(stderr);
